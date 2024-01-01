e08b6b9863d3019a0cb36cdc5791995fbb3d0eea
YJH <slade12307@gmail.com>
2024-01-01 16:34:11 +0900

    make react_ver

---
+++ self_dev_react_ver/src/App.js
@@ -0,0 +1,16 @@
+
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="https://www.acmicpc.net/" target="_blank">백준 알고리즘</a>
+    <a href="https://github.com/JaeHyunYu" target="_blank">깃허브</a>
+
+    </div>
+  );
+}
+
+export default App;
