e6d798a901f334906a878d7eb62c769207bad3c9
YJH <slade12307@gmail.com>
2024-01-01 17:45:08 +0900

    updating!

--- self_dev_react_ver/src/App.js
+++ self_dev_react_ver/src/App.js
@@ -4,11 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let imgurl="study.png"
   return (
+  
     <div className="App">
-      <a href="https://www.acmicpc.net/" target="_blank">백준 알고리즘</a>
-    <a href="https://github.com/JaeHyunYu" target="_blank">깃허브</a>
 
+      <div className='black-nav'>
+        
+        <img src={imgurl} style={{width:40, backgroundColor:'black'}}></img> &nbsp; Self_Development
+      </div>
+      <div>
+        <a href="https://www.acmicpc.net/" target="_blank">백준 알고리즘</a>
+      </div>
+      <div>      
+        <a href="https://github.com/JaeHyunYu" target="_blank">깃허브</a>
+      </div>
     </div>
   );
 }
