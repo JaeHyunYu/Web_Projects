a567fe196486acf9740a8608240cf47d07807e9b
YJH <slade12307@gmail.com>
2024-01-08 19:34:42 +0900

    update react ver

--- self_dev_react_ver/src/App.js
+++ self_dev_react_ver/src/App.js
@@ -1,28 +1,44 @@
-
-import React,{useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  let imgurl="study.png"
-  let bjurl="baekjoon.png"
-  let giturl="git.png"
+  let imgurl = "study.png";
+  let bjurl = "baekjoon.png";
+  let giturl = "git.png";
+  let blogurl = "blog.png";
   return (
-  
     <div className="App">
-
-      <div className='black-nav'>
-        
-        <img src={imgurl} style={{width:40, backgroundColor:'black'}}></img> &nbsp; Self_Development
+      <div className="black-nav">
+        <img src={imgurl} style={{ width: 40 }}></img>
+        Self_Development
       </div>
-      <div>
 
-      
-        <a href="https://www.acmicpc.net/" target="_blank"><img src={bjurl}></img>백준 알고리즘</a>
-      </div>
-      <div>      
-      
-        <a href="https://github.com/JaeHyunYu" target="_blank">  <img src={giturl}></img>깃허브</a>
+      <div className="sites">
+        <div className="item">
+          <a href="https://www.acmicpc.net/" target="_blank">
+            <div className="imgbox">
+              <img src={bjurl}></img>
+            </div>
+            <p>Algorithm</p>
+          </a>
+        </div>
+        <div className="item">
+          <a href="https://github.com/JaeHyunYu" target="_blank">
+            <div className="imgbox">
+              <img src={giturl}></img>
+            </div>
+            <p>Github</p>
+          </a>
+        </div>
+        <div className="item">
+          <a href="https://yu-dev-record.tistory.com/" target="_blank">
+            <div className="imgbox">
+              <img src={blogurl}></img>
+            </div>
+            <p>Blog</p>
+          </a>
+        </div>
       </div>
     </div>
   );
