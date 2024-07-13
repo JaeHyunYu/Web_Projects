409fc8bf6d941a8647d1c1d066179fe6081548df
YJH <slade12307@gmail.com>
2024-07-13 16:13:00 +0900

    updating

--- Collection_Self_dev/self_dev_react_ver/src/index.js
+++ Collection_Self_dev/self_dev_react_ver/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+import express from "express";
+const app = express();
+
+app.listen(8000, () => {
+  console.log("8000 서버 호출");
+});
+
+app.get('*',function(요청,응답){
+응답.sendFile(path.join(_dirname,'react-project/build/index.html'))
+}) -> react에서 route 처리해주기위해서 추가
+
+*/
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
